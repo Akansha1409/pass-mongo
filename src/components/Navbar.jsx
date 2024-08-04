ea9da57144a27c6a4bf6dc39200b3568b74718df
Akansha1409 <akanshakalyan1409@gmail.com>
2024-08-04 20:53:20 +0530

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,13 +16,13 @@ const Navbar = () => {
                         <a className='hover:font-bold' href='#'>Contact</a>
                     </li>
                 </ul> */}
-                <a href="https://github.com/Akansha1409" target="_blank" rel="noopener noreferrer">
-                <button className='text-white bg-green-700 my-5 mx-2 rounded-full flex  justify-between items-center ring-white ring-1'>
-                    <img className='invert  w-10 p-1' src="/github.png" alt="github logo" />
+            <a href="https://github.com/Akansha1409" target="_blank" rel="noopener noreferrer">
+                <button className='text-white bg-green-700 my-5 mx-2 mt-10 rounded-full flex  justify-between items-center ring-white ring-1'>
+                    <img className='invert  w-10 p-1' src="public/github.png" alt="github logo" />
                     <span className='font-bold px-2'>GitHub</span>
 
                 </button>
-                </a>
+                /</a>
             </div>
         </nav>
     )
